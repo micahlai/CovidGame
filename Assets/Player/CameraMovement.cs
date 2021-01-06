4402d3b03bed6bb7340c3283f55760b013a7512b
Anonymous Shrimp <yumiswep@gmail.com>
2021-01-05 22:27:52 -0800

    Virus movement and selection

---
+++ Assets/Player/CameraMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CameraMovement : MonoBehaviour
+{
+    selected_dictionary dict;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dict = FindObjectOfType<selected_dictionary>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                foreach(var entry in dict.selectedTable)
+                {
+                    GameObject g = entry.Value;
+                    if (g.GetComponent<Virus>() != null)
+                    {
+                        NavMeshAgent a = g.GetComponent<NavMeshAgent>();
+                        NavMeshPath path = new NavMeshPath();
+                        a.CalculatePath(hit.point, path);
+                        if (path.status == NavMeshPathStatus.PathPartial)
+                        {
+
+                        }
+                        else
+                        {
+                            //a.SetPath(path);
+                            a.SetDestination(hit.point);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Player/Selection/selection_component.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class selection_component : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (GetComponent<Virus>() != null)
+        {
+            GetComponent<Renderer>().material.SetFloat("Intensity", 1);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (GetComponent<Virus>() != null)
+        {
+            GetComponent<Renderer>().material.SetFloat("Intensity", 0);
+        }
+    }
+}
